e6fda3088f05c7b17a8024a66e2d8e3c5fe41766
levkotman <leoktmn@yandex.ru>
2021-09-29 19:07:56 +0200

    added prediction comp

---
+++ src/components/prediction/PredictHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>{{header}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["header"]
+    }
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
---
+++ src/components/prediction/ThePrediction.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <predict-header />
+        prediction quiz form
+        <div v-if="isLoading">loading spinner</div>
+        <div v-if="isRecording">Запись сообщения</div>
+        <span>вопрос № - 5</span>
+    </div>
+</template>
+
+<script>
+import PredictHeader from "./PredictHeader.vue";
+
+    export default {
+        components: {
+            PredictHeader,
+        },
+        data() {
+            return{
+                isLoading: true,
+                isRecording: false,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
