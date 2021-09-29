c61ea3ff9e55ec20d6a4340c2d7bb4cd80a8d655
levkotman <leoktmn@yandex.ru>
2021-09-29 02:06:35 +0200

    first layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header>
+      <p>лучшие астрологи</p>
+      <p>точность прогноза: 97%</p>
+      <img src="" alt="woman" />
+      <img src="" alt="icon1" />
+    </header>
+    <div>
+      <h2>
+        Вас беспокоит вопрос о том, когда Вы покинете этот Мир и при каких
+        обстоятельствах?
+      </h2>
+      <input type="button" value="Да" />
+      <input type="button" value="Нет" />
+      <span>Онлайн предсказание</span>
+    </div>
+    <div>
+      <img src="" alt="hands" />
+      <p>
+        Позвольте нам раскрыть эту волнующую тайну и
+        <span>с точностью определить дату и время вашей смерти,</span> а также
+        предшествующую этому событию причину
+      </p>
+    </div>
+    <div>
+      <p>
+        Многие не верят предсказаниям и мы решили доказать каждому,
+        <span>что прогноз может изменить жизнь любого человека!</span>
+      </p>
+    </div>
+    <div>
+      КВИЗ
+      <button>Позвонить и прослушать</button>
+    </div>
+    <footer>
+      <img src="" alt="руны" />
+      <p>Вы, конечно, умрете. И все, с кем вы знакомы, тоже однажды умрут.</p>
+      <img src="" alt="руны" />
+      <p>
+        TERMENI SI CONDITII: ACESTA ESTE UN SERVICIU DE DIVERTISMENT. PRIN
+        FOLOSIREA LUI DECLARATI CA AVETI 18 ANI IMPLINITI,
+      </p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
