5ce183aa0f5aa6d6180722ee2a0b9a4b61197257
levkotman <leoktmn@yandex.ru>
2021-10-01 05:15:02 +0200

    styled prediction header

--- src/components/PredictHeader.vue
+++ src/components/PredictHeader.vue
@@ -1,15 +1,53 @@
 <template>
-    <div>
-        <p>{{header}}</p>
-    </div>
+  <div>
+    <p :class="styles">{{ header }}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ["header"]
-    }
+export default {
+  props: ["header", "styles"],
+};
 </script>
 
-<style lang="postcss" scoped>
-
+<style scoped>
+div {
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  background: url("../assets/img/eye.svg") no-repeat bottom right;
+}
+p {
+  max-width: 74%;
+  margin: 0 auto 2.5rem;
+}
+.transparent {
+  font-family: "Bad Script";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  text-align: center;
+  color: #fff;
+  opacity: 0.6;
+}
+.window {
+  background-color: #fff;
+  border: 1px solid #fff;
+  box-sizing: border-box;
+  border-radius: 5px;
+  position: relative;
+  color: #202024;
+}
+.window:after,
+.window:before {
+  content: "";
+  display: block;
+  position: absolute;
+  left: 90%;
+  bottom: -20px;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-color: #fff transparent transparent;
+  border-width: 10px;
+}
 </style>
